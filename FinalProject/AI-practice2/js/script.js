1514380afdc92e9f6bfd1a1b4a6ff7ec74f3f4b6
LucaSab48 <lucasabelli@hotmail.com>
2024-04-17 13:45:31 -0400

    Final Project: Update 2
    Got what i think will be my main fighting mechanic working (user touching image making body light up). I initially tried using p5play library to have colliders with sprites, but it didnt seem to work. So i did the ol fashion mathematical route with calculating the distance between two points

--- FinalProject/AI-practice2/js/script.js
+++ FinalProject/AI-practice2/js/script.js
@@ -12,6 +12,24 @@ let video;
 let poseNet;
 let pose;
 let skeleton;
+let lineColor = {
+  r: 255,
+  g: 255, 
+  b: 255
+};
+let circle1 = {
+  x: 300,
+  y: 100,
+  width: 100,
+  height: 100,
+  isTouching: false,
+  img: undefined
+}
+
+
+function preload() {
+  circle1.img = loadImage("assets/images/robotBully1.png");
+}
 
 
 function setup() {
@@ -45,8 +63,10 @@ function loading() {
 
 function draw() {
   image(video, 0, 0);
+  drawCircle1();
 
   if (pose) {
+    let d = dist(pose.leftWrist.x, pose.leftWrist.y, circle1.x, circle1.y);
     for (let i = 0; i < pose.keypoints.length; i++) {
       let x = pose.keypoints[i].position.x;
       let y = pose.keypoints[i].position.y;
@@ -58,11 +78,26 @@ function draw() {
       let a = skeleton[i][0];
       let b = skeleton[i][1];
       strokeWeight(2);
-      stroke(0);
+      stroke(lineColor.r, lineColor.g, lineColor.b);
       line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
+
+    if(d < circle1.height) {
+      lineColor.r = 0;
+      lineColor.g = 255;
+      lineColor.b = 0;
+    }
+    else {
+      lineColor.r = 255;
+      lineColor.g = 255;
+      lineColor.b = 255;
     }
   }
 }
 
+
+function drawCircle1() {
+  image(circle1.img, circle1.x, circle1.y, circle1.width, circle1.height);
+}
 
 
